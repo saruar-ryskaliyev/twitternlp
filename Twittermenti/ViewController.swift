ace8ed2a22fa41964dff4d474b5ee7eb74aa8183
saruar-ryskaliyev <108226977+saruar-ryskaliyev@users.noreply.github.com>
2023-07-21 12:31:36 +0600

    Add files via upload

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
